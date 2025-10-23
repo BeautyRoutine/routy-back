cd396e394426e602f395d353fcbc2d8b84d513d2
김지용 (jiyong Kim) <111142113+j4drgn@users.noreply.github.com>
2025-10-23 18:50:52 +0900

    initialize project structure with Maven and Spring Boot setup

---
+++ src/main/java/com/routy/routyback/RoutyBackApplication.java
@@ -0,0 +1,13 @@
+package com.routy.routyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutyBackApplication.class, args);
+    }
+
+}
