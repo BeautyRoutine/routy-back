1820c5b0fe1aca783f5cc804b7fd1ead3b37f487
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-11-04 11:51:36 +0900

    1104 before lunch

--- src/main/java/com/routy/routyback/service/IProductService.java
+++ src/main/java/com/routy/routyback/service/IProductService.java
@@ -1,18 +1,38 @@
 package com.routy.routyback.service;
 
+import java.util.ArrayList;
+
+import com.routy.routyback.domain.ProductVO;
+
 public interface IProductService {
 	
-	// 상품 crud 기능
+	// 전체 조회
+	ArrayList<ProductVO> getAll();
 	
-		// 상품 재고 수정
+	// 특정 상품 조회	
+	ProductVO getById(int prdNo);
+	/* 상품 crud 기능 */
 	
-		// 상품 상태 수정
-		
-	// 상품 페이징 처리
+	// 상품 추가
+	public void insertProduct();
+	// 상품 수정 
+	public void updateProduct();
+	// 상품 삭제	
+	public void deleteProduct();
+	// 상품 재고 수정 : 
+	public void updateStock();
+	// 상품 상태 수정
+	public void updateStatus();
+	// 상품 페이징 처리 : 
 	
-	// 상품 랭킹 수정 
-	
+	// 상품 랭킹 수정
+	public void ranking();
 	// 성분 crud 기능
-	
-	// 상품 통계 조회 
+	// 상품 추가
+	public void insertIngredient();
+	// 성분 수정 
+	public void updateIngredient();
+	// 성분 삭제	
+	public void deleteIngredient();
+	// 상품 통계 조회
 }
--- src/main/java/com/routy/routyback/service/ProductService.java
+++ src/main/java/com/routy/routyback/service/ProductService.java
@@ -1,6 +1,76 @@
 package com.routy.routyback.service;
 
+import java.util.ArrayList;
+
+import com.routy.routyback.domain.ProductVO;
+
 public class ProductService implements IProductService {
+
+	@Override
+	public ArrayList<ProductVO> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductVO getById(int prdNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertProduct() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateProduct() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteProduct() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateStock() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateStatus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ranking() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertIngredient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateIngredient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteIngredient() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 }
