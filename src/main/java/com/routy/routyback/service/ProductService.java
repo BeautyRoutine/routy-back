fd18c3a0f2aac91d3fa1fefecd87010726d2c9e6
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-11-06 17:42:20 +0900

    [feature] product service , mapper , adminproductcontroller,

--- src/main/java/com/routy/routyback/service/ProductService.java
+++ src/main/java/com/routy/routyback/service/ProductService.java
@@ -2,58 +2,64 @@ package com.routy.routyback.service;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.routy.routyback.dto.ProductDTO;
+import com.routy.routyback.mapper.admin.ProductMapper;
 
 @Service
 public class ProductService implements IProductService {
 
+	@Autowired
+	ProductMapper productMapper;
+
 	@Override
 	public ArrayList<ProductDTO> getAll() {
-		// TODO 상품 조회
-		return null;
+		return productMapper.getAll();
 	}
 
 	@Override
 	public ProductDTO getById(int prdNo) {
-		// TODO id로 상품 검색
-		return null;
+		return productMapper.selectById(prdNo);
 	}
 
 	@Override
+	@Transactional
 	public void updateStock(int prdNo, int amount) {
-		// TODO 상품 수량 로직
+		ProductDTO currentProduct = productMapper.selectById(prdNo);
 
+		if (currentProduct == null) {
+			throw new IllegalArgumentException("상품 번호 " + prdNo + "에 해당하는 상품이 없습니다.");
+		}
+
+		int newStock = currentProduct.getPrdStock() + amount;
+
+		if (newStock < 0) {
+			throw new RuntimeException("상품 [" + currentProduct.getPrdName() + "]의 재고가 부족합니다.");
+		}
+
+		productMapper.productUpdateStock(prdNo, newStock);
 	}
 
 	@Override
 	public void updateStatus(int prdNo, String status) {
-		// TODO Auto-generated method stub
-
+		productMapper.productUpdateStatus(prdNo, status);
 	}
 
 	@Override
-	public void insertProduct(ProductDTO vo) {
-		// TODO 상품 추가 로직
-
+	public void insertProduct(ProductDTO product) {
+		productMapper.productInsert(product);
 	}
 
 	@Override
-	public void updateProduct(ProductDTO vo) {
-		// TODO 상품 업데이트 로직
+	public void updateProduct(ProductDTO product) {
+		productMapper.productUpdate(product);
 	}
 
 	@Override
 	public void deleteProduct(int prdNo) {
-		// TODO 상품 삭제 로직
-
+		productMapper.productDelete(prdNo);
 	}
-
-	@Override
-	public void updateRanking() {
-		// TODO 랭킹 업데이트에 대한 로직
-
-	}
-
 }
