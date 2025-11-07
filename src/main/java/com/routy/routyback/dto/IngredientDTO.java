720bf558cfe3b134b611501bf02981cbcb2e115f
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-11-07 17:31:17 +0900

    [fix]사용되지 않는 부분 제거

--- src/main/java/com/routy/routyback/dto/IngredientDTO.java
+++ src/main/java/com/routy/routyback/dto/IngredientDTO.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 public class IngredientDTO {
 	private int ingNo; // 성분 번호 (PK)
@@ -17,58 +16,75 @@ public class IngredientDTO {
 	private String ingFunctional; // 기능성 (예: 여드름 완화)
 	private Integer ingGrpNo; // 그룹 번호 (nullable)
 	private Date ingRegDate; // 등록일 (CURRENT_TIMESTAMP)
-	
+
 	public int getIngNo() {
 		return ingNo;
 	}
+
 	public void setIngNo(int ingNo) {
 		this.ingNo = ingNo;
 	}
+
 	public String getIngName() {
 		return ingName;
 	}
+
 	public void setIngName(String ingName) {
 		this.ingName = ingName;
 	}
+
 	public String getIngEnName() {
 		return ingEnName;
 	}
+
 	public void setIngEnName(String ingEnName) {
 		this.ingEnName = ingEnName;
 	}
+
 	public String getIngDesc() {
 		return ingDesc;
 	}
+
 	public void setIngDesc(String ingDesc) {
 		this.ingDesc = ingDesc;
 	}
+
 	public int getIngAllergen() {
 		return ingAllergen;
 	}
+
 	public void setIngAllergen(int ingAllergen) {
 		this.ingAllergen = ingAllergen;
 	}
+
 	public int getIngDanger() {
 		return ingDanger;
 	}
+
 	public void setIngDanger(int ingDanger) {
 		this.ingDanger = ingDanger;
 	}
+
 	public String getIngFunctional() {
 		return ingFunctional;
 	}
+
 	public void setIngFunctional(String ingFunctional) {
 		this.ingFunctional = ingFunctional;
 	}
+
 	public Integer getIngGrpNo() {
 		return ingGrpNo;
 	}
+
 	public void setIngGrpNo(Integer ingGrpNo) {
 		this.ingGrpNo = ingGrpNo;
 	}
+
 	public Date getIngRegDate() {
 		return ingRegDate;
 	}
+
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	public void setIngRegDate(Date ingRegDate) {
