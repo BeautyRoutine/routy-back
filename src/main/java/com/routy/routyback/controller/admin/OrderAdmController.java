b39f13600f1dacbfe6d23ea28f51768e760b536f
Mo Ran Huy <ranhuy.mo@gmail.com>
2025-11-10 18:26:56 +0900

    feat: 주문목록 조회 REST API 추가3
    프론트 연결
    
    resolves: #11

--- src/main/java/com/routy/routyback/controller/admin/OrderAdmController.java
+++ src/main/java/com/routy/routyback/controller/admin/OrderAdmController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.routy.routyback.service.admin.OrderAdmService;
 
+@CrossOrigin("*")
 @RestController
 public class OrderAdmController {
 	@Autowired
