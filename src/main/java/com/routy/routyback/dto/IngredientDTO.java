fd1719fb76e0f945118ac406db417f598c630bad
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-11-11 12:59:11 +0900

    [feat] lombok 사용 및 pom.xml 보안 의존성 추가.

--- src/main/java/com/routy/routyback/dto/IngredientDTO.java
+++ src/main/java/com/routy/routyback/dto/IngredientDTO.java
@@ -6,6 +6,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.Data;
+
+@Data
 public class IngredientDTO {
 	private int ingNo; // 성분 번호 (PK)
 	private String ingName; // 성분 한글 이름
@@ -15,79 +18,9 @@ public class IngredientDTO {
 	private int ingDanger; // 주의 성분 여부 (0/1)
 	private String ingFunctional; // 기능성 (예: 여드름 완화)
 	private Integer ingGrpNo; // 그룹 번호 (nullable)
-	private Date ingRegDate; // 등록일 (CURRENT_TIMESTAMP)
-
-	public int getIngNo() {
-		return ingNo;
-	}
-
-	public void setIngNo(int ingNo) {
-		this.ingNo = ingNo;
-	}
-
-	public String getIngName() {
-		return ingName;
-	}
-
-	public void setIngName(String ingName) {
-		this.ingName = ingName;
-	}
-
-	public String getIngEnName() {
-		return ingEnName;
-	}
-
-	public void setIngEnName(String ingEnName) {
-		this.ingEnName = ingEnName;
-	}
-
-	public String getIngDesc() {
-		return ingDesc;
-	}
-
-	public void setIngDesc(String ingDesc) {
-		this.ingDesc = ingDesc;
-	}
-
-	public int getIngAllergen() {
-		return ingAllergen;
-	}
-
-	public void setIngAllergen(int ingAllergen) {
-		this.ingAllergen = ingAllergen;
-	}
-
-	public int getIngDanger() {
-		return ingDanger;
-	}
-
-	public void setIngDanger(int ingDanger) {
-		this.ingDanger = ingDanger;
-	}
-
-	public String getIngFunctional() {
-		return ingFunctional;
-	}
-
-	public void setIngFunctional(String ingFunctional) {
-		this.ingFunctional = ingFunctional;
-	}
-
-	public Integer getIngGrpNo() {
-		return ingGrpNo;
-	}
-
-	public void setIngGrpNo(Integer ingGrpNo) {
-		this.ingGrpNo = ingGrpNo;
-	}
-
-	public Date getIngRegDate() {
-		return ingRegDate;
-	}
-
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-	public void setIngRegDate(Date ingRegDate) {
-		this.ingRegDate = ingRegDate;
-	}
+	private Date ingRegDate; // 등록일 (CURRENT_TIMESTAMP)
+
+
 }
