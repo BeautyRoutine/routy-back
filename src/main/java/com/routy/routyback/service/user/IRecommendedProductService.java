a7088060a4bf8477806571dafebcfbfb5eee7772
문지용 <144403071+trueisgone@users.noreply.github.com>
2025-11-28 15:05:29 +0900

    [feat] 최근 본 상품 기반 추천 API 생성 및 /list/recent 엔드포인트 추가
    # Conflicts:
    #	src/main/java/com/routy/routyback/controller/user/ProductUserController.java

--- src/main/java/com/routy/routyback/service/user/IRecommendedProductService.java
+++
@@ -1,9 +0,0 @@
-package com.routy.routyback.service.user;
-
-import com.routy.routyback.dto.RecommendedProductDTO;
-
-import java.util.List;
-
-public interface IRecommendedProductService {
-    List<RecommendedProductDTO> getRecommendedProducts(Integer userNo, List<Integer> recentPrdNos);
-}
