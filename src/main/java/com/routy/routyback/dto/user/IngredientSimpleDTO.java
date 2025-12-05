31a0e2b6d6e155372af9b6447c88340bb1c46a60
ParkMinWoo <pminwoo001@naver.com>
2025-12-05 18:14:21 +0900

    fix : 성분 dto 수정
    성분 dto 중에서 성분설명, 상품 dto에서 상세정보에서 쓸 dto 추가

--- src/main/java/com/routy/routyback/dto/user/IngredientSimpleDTO.java
+++ src/main/java/com/routy/routyback/dto/user/IngredientSimpleDTO.java
@@ -1,5 +1,7 @@
 package com.routy.routyback.dto.user;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
@@ -10,4 +12,9 @@ public class IngredientSimpleDTO {
     private int ingAllergen;     // 알레르기 여부 (1:주의, 0:안전)
     private int ingDanger;       // 위험 성분 여부 (1:주의, 0:안전)
     private String ingFunctional; // 기능성 내용
+    private String ingDesc;
 }
+
+
+
+
