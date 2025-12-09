956d9311f4e77908fe57eaf1dce548671bed9852
ParkMinWoo <pminwoo001@naver.com>
2025-12-09 14:06:45 +0900

    fix : 추후 기능 대비 안전성
    Transactional 추가로 리뷰에 좋아요 개수 카운트하는 경우에 대한 위험을 대비할 수 있음. 현재는 필요없음.

--- src/main/java/com/routy/routyback/service/user/ReviewService.java
+++ src/main/java/com/routy/routyback/service/user/ReviewService.java
@@ -15,8 +15,10 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service("reviewService")
+@Transactional
 public class ReviewService implements IReviewService {
 
     @Autowired
